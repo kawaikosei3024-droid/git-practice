6d0a34d3f065f4c63493f2ec291500bec69080c9
Kosei Kawai <kawaikosei3024@gmail.com>
2026-01-23 21:44:42 +0900

    change greeting based on time

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
-def greet(name):
-    print(f"Hello,{name}!")
+import sys
+from datetime import datetime
+def get_greeting():
+    hour = datetime.now().hour
+    if 5<= hour <12:
+        return "Good morning"
+    if 12<= hour <18:
+        return "Hello"
+    else:
+        return "Good evening"
+def greet(name: str) ->None:
+    print(f"{get_greeting}, {name}!")
 if __name__ == "__main__":
-    greet("Git")
+    if len(sys.argv) > 1:
+        name = sys.argv[1]
+    else:
+        name = "Git"
+    greet(name)
+    
